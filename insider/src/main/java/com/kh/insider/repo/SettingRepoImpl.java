874d022bab14f6259126c9df842c7e3489d2500e
hangsuu <ohhangsu@naver.com>
2023-05-09 21:11:31 +0900

    repo impl 수정

--- insider/src/main/java/com/kh/insider/repo/SettingRepoImpl.java
+++ insider/src/main/java/com/kh/insider/repo/SettingRepoImpl.java
@@ -4,8 +4,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.kh.insider.dto.SettingDto;
-
 @Repository
 public class SettingRepoImpl implements SettingRepo{
 
