b241ca2b06473ab02227ad568a574c009f89a80a
CHOONSIM <121531868+CHOONSIM@users.noreply.github.com>
2023-05-09 18:02:56 +0900

    다중 파일 업로드 구현중

--- insider/src/main/java/com/kh/insider/repo/AttachmentRepo.java
+++ insider/src/main/java/com/kh/insider/repo/AttachmentRepo.java
@@ -3,13 +3,14 @@ package com.kh.insider.repo;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.insider.dto.AttachmentDto;
 
 public interface AttachmentRepo {
 
-	void upload(MultipartFile attachment) throws IllegalStateException, IOException;
+	void upload(@RequestParam MultipartFile attachment) throws IllegalStateException, IOException;
 	List<AttachmentDto>selectList();
 
 }
