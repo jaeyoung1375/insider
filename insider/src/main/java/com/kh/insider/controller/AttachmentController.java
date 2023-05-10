f92c40694252e99f1ca349e1c156d99a14543726
CHOONSIM <121531868+CHOONSIM@users.noreply.github.com>
2023-05-10 18:13:08 +0900

    fix-게시물 등록

--- insider/src/main/java/com/kh/insider/controller/AttachmentController.java
+++ insider/src/main/java/com/kh/insider/controller/AttachmentController.java
@@ -3,10 +3,8 @@ package com.kh.insider.controller;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
 
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +14,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,7 +23,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.insider.configuration.FileUploadProperties;
-import com.kh.insider.dto.AttachmentDto;
 import com.kh.insider.repo.AttachmentRepo;
 
 import lombok.extern.slf4j.Slf4j;
@@ -71,22 +67,22 @@ public class AttachmentController {
 				.body(resource);
 	}
 	
-//	@GetMapping("/upload")
-//	public String upload() {
-//		return "attachment/upload";
-//	}
-//	
-//	@PostMapping("/upload")
-//	public String upload(@RequestParam List<MultipartFile> attaches) {
-//		log.debug("전송갯수 = " + attaches.size());
-//		return "redirect:/list";
-//	}
-//
-//	
-//	@GetMapping("/list")
-//	public String list(){
-//		return "attachment/list";
-//	}
+	@GetMapping("/upload")
+	public String upload() {
+		return "attachment/upload";
+	}
+	
+	@PostMapping("/upload")
+	public String upload(@RequestParam List<MultipartFile> attaches) {
+		log.debug("전송갯수 = " + attaches.size());
+		return "redirect:/list";
+	}
+
+	
+	@GetMapping("/list")
+	public String list(){
+		return "attachment/list";
+	}
 	
 //	private File dir;
 //	@PostConstruct
