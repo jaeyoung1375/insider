dbde076e049ca6c0e3a4485517eec5591486c1de
jaeyoung1375 <jaeyoung1375@naver.com>
2023-05-12 12:30:43 +0900

    구글 로그인 수정

--- insider/src/main/java/com/kh/insider/dto/MemberDto.java
+++ insider/src/main/java/com/kh/insider/dto/MemberDto.java
@@ -1,6 +1,8 @@
 package com.kh.insider.dto;
 
-import java.sql.Date;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import lombok.Data;
 
@@ -22,5 +24,21 @@ public class MemberDto {
 	private String memberTel;
 	private String memberGender;
 	private int memberReport;
-	private Date memberBirth;
+	private String memberBirth;
+	
+//	 public String getMemberBirth() {
+//	        return memberBirth;
+//	    }
+//	 
+//	 public void setMemberBirth(String memberBirth) {
+//	        // String 값을 Date로 변환
+//	        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//	        try {
+//	            Date birthDate = (Date) dateFormat.parse(memberBirth);
+//	            this.memberBirth = dateFormat.format(birthDate); // Optional: 날짜 형식을 유지하고 싶을 경우
+//	        } catch (ParseException e) {
+//	            e.printStackTrace();
+//	            // 날짜 변환 실패 시 예외 처리
+//	        }
+//	    }
 }
