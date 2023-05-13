b60b9e818369a9c73a3727f906d472bf74a682c3
Moon-Tree <ellaya1012@gmail.com>
2023-05-13 19:49:30 +0900

    dmUser 사용자 퇴장 메서드 Repo 및 mapper 추가

--- insider/src/main/java/com/kh/insider/controller/DmController.java
+++ insider/src/main/java/com/kh/insider/controller/DmController.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.insider.repo.DmRoomRepo;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @Controller
 @RequestMapping("/dm")
 public class DmController {
