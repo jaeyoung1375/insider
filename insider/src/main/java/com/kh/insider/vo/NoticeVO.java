43a5d3f0b1024cfc41c118f11791351af6eaecbf
CHOONSIM <121531868+CHOONSIM@users.noreply.github.com>
2023-06-02 16:33:08 +0900

    notice 알람 관련 내용 수정

--- insider/src/main/java/com/kh/insider/vo/NoticeVO.java
+++ insider/src/main/java/com/kh/insider/vo/NoticeVO.java
@@ -10,40 +10,44 @@ import lombok.NoArgsConstructor;
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class NoticeVO {
 	
-//	//유저번호
-//	private Long memberNo;
-//	//유저닉네임
-//	private String memberNick;
-//	//첨부파일
-//	private int attachmentNo;
-//	//게시물 번호
-//	private int boardNo;
-//	//게시물 시간
-//	private Date boardTime;
-//	//게시물 좋아요 시간
-//	private Date boardLikeTime;
-//	//댓글번호
-//	private int replyNo;
-//	//댓글 시간
-//	private Date replyTime;
-//	//댓글 좋아요
-//	private int replyLike;
-//	//댓글 좋아요 시간
-//	private Date replyLikeTime;
-//	//팔로워
-//	private long followFollower;
-//	//팔로우 시간
-//	private Date followTime;
-//	
-//	private Date alramTime;
-//	private int type;
-//	private int etc;
-	
-	private int boardNo;
-	private int memberNo;
+	//유저번호
+	private Long memberNo;
+	//유저닉네임
 	private String memberNick;
+	//첨부파일
 	private int attachmentNo;
-	private int check;
-	private Date alarmTime;
-	private int type;
+	//게시물 번호
+	private int boardNo;
+	//게시물 시간
+	private Date boardTime;
+	//게시물 좋아요 시간
+	private Date boardLikeTime;
+	//댓글번호
+	private int replyNo;
+	//댓글 시간
+	private Date replyTime;
+	//댓글 좋아요
+	private int replyLike;
+	//댓글 좋아요 시간
+	private Date replyLikeTime;
+	//팔로워
+	private long followFollower;
+	//팔로우 시간
+	private Date followTime;
+    //게시물 좋아요 체크
+    private int boardLikeCheck;
+	//댓글 체크
+	private int replyCheck;
+    //댓글 좋아요 체크
+    private int replyLikeCheck;
+    //팔로우 체크
+    private int followCheck;
+
+//	private int boardNo;
+//	private int memberNo;
+//	private String memberNick;
+//	private int attachmentNo;
+//	private int check;
+//	private Date alarmTime;
+//	private int type;
 }
