2051db204c0ad83261207e2fa267c1a8018d10f3
Moon-Tree <ellaya1012@gmail.com>
2023-06-04 13:46:20 +0900

    dm 읽지 않는 메세지 수 시도 실패 _read_time과 자료형이 맞지 않아서 message_send_time과 join_time으로 우선 비교한 코드

--- insider/src/main/java/com/kh/insider/repo/DmUserRepoImpl.java
+++ insider/src/main/java/com/kh/insider/repo/DmUserRepoImpl.java
@@ -1,6 +1,8 @@
 package com.kh.insider.repo;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -58,14 +60,26 @@ public class DmUserRepoImpl implements DmUserRepo {
 		return sqlSession.selectOne("dmUser.countUsersInRoom", roomNo);
 	}
 
-	///////////////////////////////////////////////////////
-	
 	@Override
-	public List<DmUserDto> findUsersByRoomNo(int roomNo) {
-		return sqlSession.selectList("dmUser.findUsersByRoomNo", roomNo);
+	public List<DmUserDto> getUnreadMessageNum(long memberNo, int roomNo) {
+	    Map<String, Object> params = new HashMap<>();
+	    params.put("memberNo", memberNo);
+	    params.put("roomNo", roomNo);
+	    return sqlSession.selectList("dmUser.getUnreadMessageNum", params);
+	}
+	
+	
+	///////////////////////////////////////////////     //읽지 않은 메세지 수   --------   삭제 예정
+
+	@Override
+	public void insertUnReadDm(DmUserDto dmUserDto) {
+		sqlSession.insert("dmUser.insertUnReadDm", dmUserDto);
 	}
 
+	@Override
+	public void updateUnReadDm(DmUserDto dmUserDto) {
+		sqlSession.update("dmUser.updateUnReadDm", dmUserDto);
+	}
 
-
-
+	
 }
