7799395b5efee911185f316a3013a0a5a99d3707
jaeyoung1375 <79030661+jaeyoung1375@users.noreply.github.com>
2023-06-10 13:49:51 +0900

    비밀번호 암호화 적용

--- insider/src/main/java/com/kh/insider/service/MemberService.java
+++ insider/src/main/java/com/kh/insider/service/MemberService.java
@@ -1,15 +1,14 @@
 package com.kh.insider.service;
 
+import com.kh.insider.dto.MemberDto;
+
 public interface MemberService {
+	public void join(MemberDto memberDto);
+	public MemberDto login(String memberEmail, String memberPassword); 
+	public boolean checkPassword(String enteredPassword, String encodedPassword);
 	public int RandomCode();
 	public String generatTempPassword(); // 임시비밀번호 생성
 	public int sendEmail(String email);
+	public void changePassword(MemberDto memberDto);
 	
-	/*
-	 * 1. 사용자가 이메일을 입력하고 이메일 전송 버튼을 누른다.
-	 * 2. 사용자가 전송된 인증번호를 입력한다.
-	 * 3. 인증번호가 일치하면 임시비밀번호를 해당 계정의 비밀번호를 임시비밀번호로 변경한다.
-	 * 4. 사용자에게 임시비밀번호를 보여준다.
-	 * 
-	 */
 }
