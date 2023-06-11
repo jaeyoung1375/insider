3ad125ef7a2e3b033dd45984ee0c3b5283e1fa9c
Moon-Tree <ellaya1012@gmail.com>
2023-06-11 21:02:47 +0900

    dm header에서 읽지 않은 메세지 알림 수정

--- insider/src/main/java/com/kh/insider/service/NoticeService.java
+++ insider/src/main/java/com/kh/insider/service/NoticeService.java
@@ -8,8 +8,6 @@ public interface NoticeService {
 
 	List<NoticeVO> selectNotice(Long memberNo);
 	void check(Long memeberNo);
-	//dm 알림
-	int isDm(Long memberNo);
 //	List<NoticeVO> fetchUpdates(Long memberNo);
 //	void cleanup(Long memberNo);
 }
