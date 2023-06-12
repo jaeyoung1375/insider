1c478fefebb0b2cae108915da758f295b13a44c7
hangsuu <ohhangsu@naver.com>
2023-06-12 20:42:23 +0900

    인터셉터, 로그인 세션 제거

--- insider/src/main/java/com/kh/insider/configuration/InterceptorConfiguration.java
+++ insider/src/main/java/com/kh/insider/configuration/InterceptorConfiguration.java
@@ -1,47 +1,47 @@
-//package com.kh.insider.configuration;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import com.kh.insider.interceptor.AdminInterceptor;
-//import com.kh.insider.interceptor.MemberNonLoginInterceptor;
-//
-//@Configuration
-//public class InterceptorConfiguration implements WebMvcConfigurer {
-//	@Autowired
-//	private MemberNonLoginInterceptor memberNonLoginInterceptor;
-//	@Autowired
-//	private AdminInterceptor adminInterceptor;
-//	@Override
-//	public void addInterceptors(InterceptorRegistry registry) {
-//		
-//		// 1. MemberNonLoginInterceptor
-//		registry.addInterceptor(memberNonLoginInterceptor)
-//				.addPathPatterns(
-//						"/**"
-//						)
-//				.excludePathPatterns(
-//						"/member/login/**",
-//						"/member/logout",
-//						"/member/join/**",
-//						"/member/addInfo",
-//						"/member/auth/**",
-//						"/member/emailCheck",
-//						"/member/nickCheck",
-//						"/member/sendMail",
-//						"/member/passwordChange",	
-//						"/member/suspension",
-//						"/member/passwordSearch",
-//						"/member/resetPassword",
-//						"/member/sendMail",
-//						"/member/checkCert",
-//						"/member/passwordChange",
-//						"/static/**",
-//						"/kakao/login",
-//						"/google/login"
-//						);
+package com.kh.insider.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.kh.insider.interceptor.AdminInterceptor;
+import com.kh.insider.interceptor.MemberNonLoginInterceptor;
+
+@Configuration
+public class InterceptorConfiguration implements WebMvcConfigurer {
+	@Autowired
+	private MemberNonLoginInterceptor memberNonLoginInterceptor;
+	@Autowired
+	private AdminInterceptor adminInterceptor;
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		// 1. MemberNonLoginInterceptor
+		registry.addInterceptor(memberNonLoginInterceptor)
+				.addPathPatterns(
+						"/**"
+						)
+				.excludePathPatterns(
+						"/member/login/**",
+						"/member/logout",
+						"/member/join/**",
+						"/member/addInfo",
+						"/member/auth/**",
+						"/member/emailCheck",
+						"/member/nickCheck",
+						"/member/sendMail",
+						"/member/passwordChange",	
+						"/member/suspension",
+						"/member/passwordSearch",
+						"/member/resetPassword",
+						"/member/sendMail",
+						"/member/checkCert",
+						"/member/passwordChange",
+						"/static/**",
+						"/kakao/login",
+						"/google/login"
+						);
 
 
 		//Admin 인터셉터
